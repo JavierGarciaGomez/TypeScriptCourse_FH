89a2c56495eb82fe4f65e211f6d39dc7f76ed2ab
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-03-30 21:15:32 +0200

    s02-intro

---
+++ s02_intro/bases/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let msg = "Hola, mundo";
+msg = 'hola, mundo es un string';
+console.log(msg);
+let newMsg = 'a ver';
+let msg3 = 'hola';
+console.log('************ Lesson 9 ************');
+const hero = { name: 'Spiderman', age: 45 };
+hero.age = 50;
